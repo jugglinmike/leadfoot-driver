a7f0c15a38703e74241de0842287d0e5df8f38ce
Mike Pennisi <mike@mikepennisi.com>
2015-11-01 12:38:12 -0500

    Improve documentation

--- src/wait-for.js
+++ src/wait-for.js
@@ -4,15 +4,17 @@
  * Continuously invoke some Promise-returning function until one of the
  * following events occurs:
  *
- * - the Promise it returns resolves successfully
+ * - the Promise returned by the function is resolved or rejected
  * - a certain amount of time has passed
  *
  * @param {Function} conditionFn should return a Promise with each invocation.
  *                               When the Promise is rejected, polling will
- *                               continue (unless the timoue has been reached).
+ *                               continue (unless the timout has been reached).
  *                               When the Promise is fulfilled, the Promise
  *                               returned by this function will also be
- *                               fulfilled.
+ *                               fulfilled. When the Promise is rejected, the
+ *                               Promise returned by this function will also be
+ *                               rejected using the same "reason" value.
  * @param {Object} [options]
  * @param {number} [options.timeout] the amount of time to wait before
  *                                   considering the polling operation "failed"
